a2daf0076a3bb81fa6c9832e93dd8469b90eb3a3
GowthamRayar <gsgowtham1990@gmail.com>
2021-09-30 11:02:54 +0530

    Commiting source for DialoguesAndNotifications

---
+++ CodeLabs/DialoguesAndNotifications/entry/src/ohosTest/java/com/example/dialoguesandnotifications/ExampleOhosTest.java
@@ -0,0 +1,14 @@
+package com.example.dialoguesandnotifications;
+
+import ohos.aafwk.ability.delegation.AbilityDelegatorRegistry;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleOhosTest {
+    @Test
+    public void testBundleName() {
+        final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
+        assertEquals("com.example.dialoguesandnotifications", actualBundleName);
+    }
+}
