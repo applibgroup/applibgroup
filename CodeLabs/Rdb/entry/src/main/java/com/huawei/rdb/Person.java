bea0916efb7da4ef52d553fd07b223b4fbf95edb
GowthamRayar <gsgowtham1990@gmail.com>
2021-10-22 17:55:31 +0530

    Added completed Rdb project

---
+++ CodeLabs/Rdb/entry/src/main/java/com/huawei/rdb/Person.java
@@ -0,0 +1,14 @@
+package com.huawei.rdb;
+
+public class Person {
+    String name;
+    String lastName;
+
+    public Person() {
+    }
+
+    public String getName() {return name;}
+    public String getLastName() { return lastName;}
+    public void setName(String name) { this.name = name;}
+    public void setLastName(String lastName) { this.lastName = lastName;}
+}
